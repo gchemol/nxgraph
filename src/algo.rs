f709ff21cd24d2522fa2716a2976a8a5c29fdbaa
Wenping Guo <ybyygu@gmail.com>
2023-01-27 10:46:01 +0800

    subgraph: fix node order issue

--- src/algo.rs
+++ src/algo.rs
@@ -31,8 +31,9 @@ where
         }
     }
 
-    /// Returns a subgraph induced on `nodes`. The induced subgraph of the graph
-    /// contains the nodes in `nodes` and the edges between those nodes.
+    /// Returns a subgraph induced on `nodes`. The induced subgraph of
+    /// the graph contains the nodes in `nodes` in the same order and
+    /// the edges between those nodes.
     ///
     /// # Example
     ///
@@ -57,38 +58,60 @@ where
     ///
     /// * https://networkx.github.io/documentation/stable/reference/classes/generated/networkx.Graph.subgraph.html
     pub fn subgraph(&self, nodes: &[NodeIndex]) -> Self {
+        use std::collections::{HashMap, HashSet};
+
         // sanity check
-        let nodes_set: std::collections::HashSet<_> = nodes.iter().copied().collect();
+        let nodes_set: HashSet<_> = nodes.iter().copied().collect();
         assert_eq!(nodes_set.len(), nodes.len(), "nodes have duplicate {nodes:?}");
 
-        let graph = self.raw_graph().filter_map(
-            // map nodes
-            |n, node_data: &N| {
-                if nodes_set.contains(&n) {
-                    Some(node_data.clone())
-                } else {
-                    None
-                }
-            },
-            // map edges
-            |e, edge_data: &E| Some(edge_data.clone()),
-        );
+        let edges_ = self.edges();
+        let nodes_ = self.nodes();
+        let mut subgraph = Self::new();
+        // add nodes
+        // mapping old node index => new node index
+        let map: HashMap<_, _> = nodes
+            .iter()
+            .map(|&n| {
+                let n_new = subgraph.add_node(nodes_[n].clone());
+                (n, n_new)
+            })
+            .collect();
 
-        Self::from_raw_graph(graph)
+        // add edges
+        for &i in nodes {
+            for j in self.neighbors(i) {
+                if j > i && nodes.contains(&j) {
+                    let eij = edges_[(i, j)].clone();
+                    subgraph.add_edge(map[&i], map[&j], eij);
+                }
+            }
+        }
+
+        subgraph
+
+        // NOTE: the follow codes simply works, but the node order of
+        // resulted graph is different from `nodes`
+
+        // let mut subgraph = self.raw_graph().clone();
+        // subgraph.retain_nodes(|_, n| nodes_set.contains(&n));
+
+        // Self::from_raw_graph(subgraph)
     }
 }
 // d40009e1 ends here
 
-// [[file:../nxgraph.note::*test][test:1]]
+// [[file:../nxgraph.note::98add588][98add588]]
 #[test]
 fn test_subgraph() {
     // subgraph
     // g: 1--2--3--4
     let mut g = NxGraph::path_graph(4);
     let nodes: Vec<_> = g.node_indices().collect();
-    let h = g.subgraph(&nodes[0..3]);
+    let h = g.subgraph(&[nodes[1], nodes[0], nodes[2]]);
     assert_eq!(h.number_of_nodes(), 3);
     assert_eq!(h.number_of_edges(), 2);
+    // subgraph should maintain the node orders
+    assert_eq!(h.nodes().map(|(_, &n)| n).collect_vec(), vec![2, 1, 3]);
 
     // subgraphs from connected components
     // g: 1--2--3--4 10--11--12
@@ -105,4 +128,4 @@ fn test_subgraph() {
     assert_eq!(nodes_data_a, vec![10, 11, 12]);
     assert_eq!(nodes_data_b, vec![1, 2, 3, 4]);
 }
-// test:1 ends here
+// 98add588 ends here
